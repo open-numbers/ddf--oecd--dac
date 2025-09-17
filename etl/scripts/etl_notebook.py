a2c860a2879a2f546acb505becc8d5c1f5faabb7
Semio Zheng <semio@posteo.net>
2025-09-17 10:20:45 +0800

    update with latest source

--- etl/scripts/etl_notebook.py
+++ etl/scripts/etl_notebook.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-"""
-
-import json
 from pathlib import Path
 
 import pandas as pd
@@ -11,81 +7,99 @@ import pandas as pd
 from ddf_utils.str import to_concept_id
 
 
-source = '../source/TABLE5.json'
-out_dir = '../../'
+source = "../source/Table5_Data.zip"
+out_dir = "../../"
+
+
+def gen_datapoints(df: pd.DataFrame) -> pd.DataFrame:
+    """Generate datapoints dataframe."""
+    cols = {
+        "AIDTYPE": "aid_type",
+        "AMOUNTTYPE": "amount_type",
+        "DONOR": "donor",
+        "SECTOR": "sector",
+        "Year": "year",
+        "Value": "oda",
+    }
+    dp = df[list(cols.keys())].copy()
+    dp = dp.rename(columns=cols)
+
+    id_cols = ["aid_type", "amount_type", "donor", "sector"]
+    for c in id_cols:
+        dp[c] = dp[c].astype(str).map(to_concept_id)
+
+    return dp.set_index(id_cols + ["year"])
+
+
+def gen_entities(df: pd.DataFrame) -> dict:
+    """Generate all entity dataframes."""
+    entity_domains = {
+        "aid_type": ["AIDTYPE", "Aid type"],
+        "amount_type": ["AMOUNTTYPE", "Amount type"],
+        "donor": ["DONOR", "Donor"],
+        "sector": ["SECTOR", "Sector"],
+    }
+    entities = {}
+    for concept, (id_col, name_col) in entity_domains.items():
+        edf = df[[id_col, name_col]].drop_duplicates().copy()
+        edf.columns = [concept, "name"]
+        edf[concept] = edf[concept].astype(str).map(to_concept_id)
+        edf = edf.sort_values(by=concept).reset_index(drop=True)
+        entities[concept] = edf
+    return entities
+
+
+def gen_concepts() -> pd.DataFrame:
+    """Generate concepts dataframe."""
+    # manually insert some concepts
+    concepts = [
+        {"concept": "oda", "concept_type": "measure", "name": "Aid (ODA) by sector and donor"},
+        {"concept": "year", "concept_type": "time", "name": "Year"},
+        {"concept": "name", "concept_type": "string", "name": "Name"},
+    ]
+    entity_domains = {
+        "aid_type": "Aid type",
+        "amount_type": "Amount type",
+        "donor": "Donor",
+        "sector": "Sector",
+    }
+    entities_concepts = []
+    for concept, name in entity_domains.items():
+        entities_concepts.append(
+            {"concept": concept, "name": name, "concept_type": "entity_domain"}
+        )
+
+    cdf = pd.DataFrame.from_records([*concepts, *entities_concepts])
+    return cdf
 
 
 def main():
-    data = json.load(open(source))
+    """main function to run ETL"""
+    # read data
+    data = pd.read_csv(source)
 
-    # create datapoints
-    dimensions = data['structure']['dimensions']
-    series = data['dataSets'][0]['series']
+    # datapoints
+    dp = gen_datapoints(data)
+    (
+        dp.to_csv(
+            Path(
+                out_dir, "ddf--datapoints--oda--by--aid_type--amount_type--donor--sector--year.csv"
+            )
+        )
+    )
 
-    index_cols = [x['name'] for x in dimensions['series']]
-
-    recs = []
-    name = data['structure']['name']
-
-    for i, v in series.items():
-        idxs = list(map(int, i.split(':')))
-        idx_dict = dict(zip(index_cols, idxs))
-        for i, k in enumerate(idx_dict.keys()):
-            assert i == dimensions['series'][i]['keyPosition']
-            idx_dict[k] = dimensions['series'][i]['values'][idx_dict[k]]['id']
-        for t, o in v['observations'].items():
-            # TODO: might be the observations index is a list?
-            year = int(dimensions['observation'][0]['values'][int(t)]['id'])
-
-            rec = {}
-            for k, v in idx_dict.items():
-                rec[k] = v
-            rec['year'] = year
-            rec[name] = o[0]
-            recs.append(rec)
-
-    rdf = pd.DataFrame.from_records(recs)
-    rdf.columns = rdf.columns.map(to_concept_id)
-    rdf.amount_type = rdf.amount_type.map(to_concept_id)  # amount_type is in uppercase, fix it
-    rdf = rdf.rename(columns={'aid_oda_by_sector_and_donor_dac5': 'oda'}) # rename the indicator
-
-    (rdf.set_index(['aid_type', 'amount_type', 'donor', 'sector', 'year'])
-        .to_csv(Path(out_dir,
-                     'ddf--datapoints--oda--by--aid_type--amount_type--donor--sector--year.csv')))
-
-    # create entities
-    entities_concepts = []
-    entities_dfs = {}
-
-    for d in dimensions['series']:
-        name = d['name']
-        concept = to_concept_id(name)
-
-        value_df = pd.DataFrame.from_records(d['values'])
-        value_df.columns = [concept, 'name']
-
-        entities_concepts.append({'concept': concept, 'name': name, 'concept_type': 'entity_domain'})
-        entities_dfs[concept] = value_df
-
-    for k, v in entities_dfs.items():
-        path = Path(out_dir, f'ddf--entities--{k}.csv')
-        v[k] = v[k].map(to_concept_id)
+    # entities
+    entities = gen_entities(data)
+    for k, v in entities.items():
+        path = Path(out_dir, f"ddf--entities--{k}.csv")
         v.to_csv(path, index=False)
 
-    # create concepts
+    # concepts
+    cdf = gen_concepts()
+    cdf.to_csv(Path(out_dir, "ddf--concepts.csv"), index=False)
 
-    # manually insert some concepts
-    concepts = [
-        {'concept': 'oda', 'concept_type': 'measure', 'name': 'Aid (ODA) by sector and donor'},
-        {'concept': 'year', 'concept_type': 'time', 'name': 'Year'},
-        {'concept': 'name', 'concept_type': 'string', 'name': 'Name'}
-    ]
-
-    cdf = pd.DataFrame.from_records([*concepts, *entities_concepts])
-    cdf.to_csv(Path(out_dir, 'ddf--concepts.csv'), index=False)
-
-    print('Done.')
+    print("Done.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
